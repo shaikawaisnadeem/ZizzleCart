db599fcc650e575fbe6f074c85aafb7ade7b48e9
awais <awaisn.offi@gmail.com>
2025-06-03 07:07:19 +0000

    addd

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -3,33 +3,40 @@ import ReactContext from '../../Context/ReactContext';
 import './AddToCart.css';
 
 const AddToCart = () => {
-  const { itemsinCart,add } = useContext(ReactContext);
-  const increament = (product)=>{
-add(product)
-  }
+  const { itemsinCart, add } = useContext(ReactContext);
+
+  const increament = (product) => {
+    add(product);
+  };
+
+  const noProduct = <h1 className='noAdded'>No Products in Cart</h1>;
+
   return (
     <div className='cartContainer'>
       <h1 className="cartHeading">Shopping Cart</h1>
-      
-      {itemsinCart.map((product, index) => (
-        <div className="cartItem" key={index}>
-          <div className="cartProductDetails">
-            <div className="cartImageWrapper">
-              <img src={product.images[0]} alt={product.title} />
+      {itemsinCart.length > 0 ? (
+        itemsinCart.map((product, index) => (
+          <div className="cartItem" key={index}>
+            <div className="cartProductDetails">
+              <div className="cartImageWrapper">
+                <img src={product.images[0]} alt={product.title} />
+              </div>
+              <div className="cartTextInfo">
+                <p>{product.category}</p>
+                <h1>{product.title}</h1>
+                <h2>$ {product.price * product.quantity}</h2>
+              </div>
             </div>
-            <div className="cartTextInfo">
-              <p>{product.category}</p>
-              <h1>{product.title}</h1>
-              <h2>$ {product.price * product.quantity}</h2>
+            <div className="cartRemoveIcon">
+              <i className="bi bi-trash3"></i>
+              <p className='quantityProduct'>Quantity: {product.quantity}</p>
+              <p className='increment-button' onClick={() => increament(product)}>+1</p>
             </div>
           </div>
-          <div className="cartRemoveIcon">
-            <i className="bi bi-trash3"></i>
-            <p className='quantityProduct'>Quantity: {product.quantity}</p>
-           <p className='increment-button' onClick={() => increament(product)}>+1</p>
-          </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        noProduct
+      )}
     </div>
   );
 };
