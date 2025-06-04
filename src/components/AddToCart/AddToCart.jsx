8787d5e3f417492d856325acf481ff0b748d18cc
awais <awaisn.offi@gmail.com>
2025-06-04 07:05:22 +0000

    wish

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,43 +1,68 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ReactContext from '../../Context/ReactContext';
 import './AddToCart.css';
+import { MoonLoader } from 'react-spinners';
 
 const AddToCart = () => {
   const { itemsinCart, add } = useContext(ReactContext);
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1); 
   const increament = (product) => {
     add(product);
   };
+
+  const [cartLoader, setcartLoader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setcartLoader(false); 
+    }, 1000); 
+    return () => clearTimeout(timer); 
+  }, []);
 
   const noProduct = <h1 className='noAdded'>No Products in Cart</h1>;
 
   return (
     <div className='cartContainer'>
       <h1 className="cartHeading">Shopping Cart</h1>
-      {itemsinCart.length > 0 ? (
-        itemsinCart.map((product, index) => (
-          <div className="cartItem" key={index}>
-            <div className="cartProductDetails">
-              <div className="cartImageWrapper">
-                <img src={product.images[0]} alt={product.title} />
-              </div>
-              <div className="cartTextInfo">
-                <p>{product.category}</p>
-                <h1>{product.title}</h1>
-                <h2>$ {(product.price * quantity).toFixed(2)}</h2>
-              </div>
-            </div>
-            <div className="cartRemoveIcon">
-              <i className="bi bi-trash3"></i>
-              {/* <p className='quantityProduct'>Quantity: {product.quantity}</p> */}
-              <p className='quantityProduct'>Quantity: <input type='number' className='quantityInput' onChange={(e)=>setQuantity(e.target.value) } value={quantity}/></p>
-              {/* <p className='increment-button' onClick={() => increament(product)}>+1</p> */}
-              
-            </div>
-          </div>
-        ))
+
+      {cartLoader ? (
+        <div className="loaderWrapper">
+          <MoonLoader  color="#2b303b" size={50} />
+        </div>
       ) : (
-        noProduct
+        <>
+          {itemsinCart.length > 0 ? (
+            itemsinCart.map((product, index) => (
+              <div className="cartItem" key={index}>
+                <div className="cartProductDetails">
+                  <div className="cartImageWrapper">
+                    <img src={product.images[0]} alt={product.title} />
+                  </div>
+                  <div className="cartTextInfo">
+                    <p>{product.category}</p>
+                    <h1>{product.title}</h1>
+                    <h2>$ {(product.price * quantity).toFixed(2)}</h2>
+                  </div>
+                </div>
+                <div className="cartRemoveIcon">
+                  <i className="bi bi-trash3"></i>
+                  <p className='quantityProduct'>
+                    Quantity: 
+                    <input
+                      type='number'
+                      className='quantityInput'
+                      onChange={(e) => setQuantity(e.target.value)}
+                      value={quantity}
+                    />
+                  </p>
+                  {/* <p className='increment-button' onClick={() => increament(product)}>+1</p> */}
+                </div>
+              </div>
+            ))
+          ) : (
+            noProduct
+          )}
+        </>
       )}
     </div>
   );
--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -1,22 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ReactContext from '../../Context/ReactContext.jsx';
 import ProductCard from '../Productcard/ProductCard';
 import './WishList.css';
+import { MoonLoader } from 'react-spinners';
 
 const WishList = () => {
   const { itemsinwish } = useContext(ReactContext);
+  const [wishLoader, setwishLoader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setwishLoader(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className='mainwishDiv'>
-      <div className='wishlistDiv'>
-        {itemsinwish.length === 0 ? (
-          <h2 className="noWishText">No items in wishlist</h2>
-        ) : (
-          itemsinwish.map(product => (
-            <ProductCard key={product.id} product={product} />
-          ))
-        )}
-      </div>
+      <h1 className='wishHeading'>WishList</h1>
+      {wishLoader ? (
+        <div className="loaderWrapper">
+          <MoonLoader color="#2b303b" size={50} />
+        </div>
+      ) : (
+        <div className='wishlistDiv'>
+          {itemsinwish.length === 0 ? (
+            <h2 className="noWishText">No items in wishlist</h2>
+          ) : (
+            itemsinwish.map(product => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 };
